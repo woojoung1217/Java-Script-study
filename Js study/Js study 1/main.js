55929fec85d01a00d10bafe1e7765abaee4c72e9
Woo joung Yoon <34205465+woojoung1217@users.noreply.github.com>
2021-07-07 20:06:40 +0900

    Add files via upload

---
+++ Js study/Js study 1/main.js
@@ -0,0 +1,13 @@
+// 문자형
+let myName= " woojoung yoon";
+let myemail= " ywj981217@naver.com";
+let hello= `hello $ {myName} ?!`;
+
+console.log(myName);
+console.log(myemail);
+console.log(hello);
+//숫자형 +곱셈 
+let number =4;
+let integer=10;
+let bis=number*integer;
+console.log(bis);
