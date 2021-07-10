5245cd0227a5e28a235052f00631869ea663c539
Woo joung Yoon <34205465+woojoung1217@users.noreply.github.com>
2021-07-10 15:06:48 +0900

    조건문

---
+++ Js study 5/main.js
@@ -0,0 +1,20 @@
+//  js study 5 조건문 
+
+let good = true;
+
+let bad= false;
+if(good) {
+    console.log("this is good");    
+}
+if(bad) {
+    console.log("this is not good");    
+}
+// else 
+
+let istrue=false;
+
+if(istrue){
+    console.log("true");
+}else{
+    console.log("not true");
+}
