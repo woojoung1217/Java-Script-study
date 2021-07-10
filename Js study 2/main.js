0ec7d66bf9dd0084aacd2f24dc0eacc9dc3b109c
Woo joung Yoon <34205465+woojoung1217@users.noreply.github.com>
2021-07-10 14:09:39 +0900

    Add files via upload

---
+++ Js study 2/main.js
@@ -0,0 +1,24 @@
+//사칙연산 
+// java script data type 
+let a=2;
+let b=5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+//변수는 재사용 가능 
+
+let c=1;
+
+console.log(c);
+
+c=999;
+
+console.log(c);
+
+const c=123;
+console.log(c);  //const 변수 사용 시 재사용이 불가   syntax error 
+
+//this , if , break  등  예약어 사용 시 syntax error 가 발생 
