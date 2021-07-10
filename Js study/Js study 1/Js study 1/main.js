ace996b67cac70fef4186fd0d79dbefd332680aa
Woo joung Yoon <34205465+woojoung1217@users.noreply.github.com>
2021-07-10 13:15:57 +0900

    Data Type 추가 array , object

---
+++ Js study/Js study 1/Js study 1/main.js
@@ -0,0 +1,33 @@
+// 문자형
+let myName= " woojoung yoon";
+let myemail= " ywj981217@naver.com";
+let hello= `hello ${myName} ?!`;
+
+console.log(myName);
+console.log(myemail);
+console.log(hello);
+//숫자형 +곱셈 
+let number =4;
+let integer=10;
+let bis=number*integer;
+console.log(bis);
+
+// obeject 
+
+let user={
+  name:"woojoung",
+  age:24,
+  isvalid:true
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.isvalid);
+
+//array 
+
+let fruits=['apple','banana','orange'];
+//zero base numbering
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
