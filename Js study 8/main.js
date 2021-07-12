148da10b298102cdb63c59841f35843da09b06e0
Woo joung Yoon <34205465+woojoung1217@users.noreply.github.com>
2021-07-12 16:00:20 +0900

    Getter , Setter

---
+++ Js study 8/main.js
@@ -0,0 +1,12 @@
+//Getter , Setter 값
+
+// 요소안에 text 값을 textcontent 로 가져와 출력 
+const boxEl = document.querySelector('.box');
+
+console.log(boxEl.textContent);
+
+//요소안에 text 값을 설정 
+
+boxEl.textContent='woojoung';
+
+console.log(boxEl.textContent);
