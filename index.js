86ce30b29b8e9dd18cd8fb81d2bc09b70944dcce
woojoung <ywj981217@naver.com>
2023-04-25 19:39:11 +0900

    common js add

--- index.js
+++ index.js
@@ -63,3 +63,55 @@
 // const eng = prompt("영어");
 // const result = (parseInt(math) + parseInt(eng)) / 2;
 // console.log(result);
+
+// Stirng 형 변환
+
+// console.log(typeof String(3));
+// console.log(typeof String(null));
+// let a = [1, 2, 3, 4, 5, 6];
+// for (let i = 0; i < a.length; i += 1) {
+//   a[i] = String(a[i]);
+//   setTimeout(() => {
+//     console.log(a[i]);
+//   }, 1000 * i);
+// }
+
+// 배열안에서 홀수 찾는 방법
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const multiplesOfThree = arr.filter((num, i) => i % 2 === 1);
+
+// console.log(multiplesOfThree); // [1,3,5,7,9]
+
+// <비교연산자>
+
+// let a = "3";
+// let b = 3;
+// console.log(a == b); // true 반환
+// // 타입 까지 반환 => ===
+
+// function agecheck(age, ages) {
+//   ages = 0;
+//   if (age > 19) {
+//     ages += 1;
+//     console.log("adult", ages);
+//   } else {
+//     console.log("kids", ages);
+//   }
+// }
+// agecheck(16);
+
+// 전역 함수로 만들기
+// 함수를 전역에 노출시키는 방법
+globalThis.AgeCheck = (ages) => {
+  if (ages > 19) {
+    return "성인";
+  } else {
+    return "미성년자";
+  }
+};
+const ReturnName = () => {
+  let name = "나는 윤우중";
+  console.log(name, AgeCheck(20), "입니다.");
+};
+ReturnName();
